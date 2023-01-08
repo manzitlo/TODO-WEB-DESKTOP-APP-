c29a7c78feca0cbe50d406296e67f0ebb5c2faeb
Wenzhe Luo <hellolwz1120@gmail.com>
2023-01-07 22:22:19 -0500

    Complete and Exit button updated

--- gui.py
+++ gui.py
@@ -3,6 +3,8 @@
 import modules.functions
 import PySimpleGUI as sg
 
+sg.theme('DarkTeal7')
+
 label = sg.Text("Type in a To-do:")
 input_box = sg.InputText(tooltip="Enter Todo to record what you wanna do...", key="todo")
 add_button = sg.Button("Add")
@@ -10,6 +12,8 @@ list_box = sg.Listbox(values=modules.functions.get_todos(), key='todos',
                       enable_events=True,
                       size=[45, 10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 # The title of the window
 """
@@ -17,16 +21,15 @@ layout should have a list inside the list! Widgets...
 """
 
 window = sg.Window('My To-do App',
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
-                   font=('Helvetica', 15))
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
+                   font=('Lucid', 15))
 
 while True:
 
     user_action, values = window.read()
-
-    print(user_action)
-    todo = values
-    print(todo)
 
     match user_action:
         case "Add":
@@ -48,6 +51,17 @@ while True:
             modules.functions.write_todos(todos)
 
             window['todos'].update(values=todos)  # update is part of listbox
+
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = modules.functions.get_todos()
+            todos.remove(todo_to_complete)
+            modules.functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+
+        case "Exit":
+            break
 
         case "todos":
             window['todo'].update(value=values['todos'][0])
