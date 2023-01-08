441ff5364a325272dad3ce9655a0ecfa46c0602e
Wenzhe Luo <hellolwz1120@gmail.com>
2023-01-07 22:38:58 -0500

    final version for desktop APP

--- gui.py
+++ gui.py
@@ -2,9 +2,11 @@
 
 import modules.functions
 import PySimpleGUI as sg
+import time
 
-sg.theme('DarkTeal7')
+sg.theme('GreenTan')
 
+clock = sg.Text('', key="clock")
 label = sg.Text("Type in a To-do:")
 input_box = sg.InputText(tooltip="Enter Todo to record what you wanna do...", key="todo")
 add_button = sg.Button("Add")
@@ -15,21 +17,25 @@ edit_button = sg.Button("Edit")
 complete_button = sg.Button("Complete")
 exit_button = sg.Button("Exit")
 
+output_label = sg.Text(key="output", text_color="red")
+
 # The title of the window
 """
 layout should have a list inside the list! Widgets...
 """
 
 window = sg.Window('My To-do App',
-                   layout=[[label],
+                   layout=[[clock],
+                           [label],
                            [input_box, add_button],
                            [list_box, edit_button, complete_button],
-                           [exit_button]],
+                           [exit_button, output_label]],
                    font=('Lucid', 15))
 
 while True:
 
-    user_action, values = window.read()
+    user_action, values = window.read(timeout=200)
+    window["clock"].update(value=time.strftime("%b %d - %Y, %H:%M:%S") + "       Made by Wenzhe")
 
     match user_action:
         case "Add":
@@ -41,24 +47,33 @@ while True:
             window['todos'].update(values=todos)
 
         case "Edit":
-            todo_to_edit = values["todos"][0]
-            new_todo = values['todo']
+            try:
+                todo_to_edit = values["todos"][0]
+                new_todo = values['todo']
 
-            todos = modules.functions.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
+                todos = modules.functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
 
-            modules.functions.write_todos(todos)
+                modules.functions.write_todos(todos)
 
-            window['todos'].update(values=todos)  # update is part of listbox
+                window['todos'].update(values=todos)  # update is part of listbox
+
+            except IndexError:
+                window["output"].update(value="Please select an item first!!")
+                # or using PySimpleGUI.popup
 
         case "Complete":
-            todo_to_complete = values['todos'][0]
-            todos = modules.functions.get_todos()
-            todos.remove(todo_to_complete)
-            modules.functions.write_todos(todos)
-            window['todos'].update(values=todos)
-            window['todo'].update(value='')
+            try:
+                todo_to_complete = values['todos'][0]
+                todos = modules.functions.get_todos()
+                todos.remove(todo_to_complete)
+                modules.functions.write_todos(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value='')
+
+            except IndexError:
+                window["output"].update(value="Please select an item first!!")
 
         case "Exit":
             break
